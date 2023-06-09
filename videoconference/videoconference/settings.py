8320b172b6779b7bf939f748368f9219c5c81572
HASSAN Abdouhraman <103179860+Abdouhramanhassanroble@users.noreply.github.com>
2023-06-09 11:37:01 +0200

    " Remis à jour "

--- videoconference/videoconference/settings.py
+++ videoconference/videoconference/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4vkw4cp4n@r0d%js(s8!a*u27b)+oyv!kwu5t6&n+s69^(!^-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
